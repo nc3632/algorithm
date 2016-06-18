be83ed2a5e75edd379fc3837f7091f6f58618235
Xuli Liu <liuxuli@gmail.com>
2016-06-18 11:10:53 -0700

    Maximal rectangle problem

---
+++ stack/MaximalRectangle.java
@@ -0,0 +1,52 @@
+import java.util.Stack;
+
+/**
+ * Given a 2D binary matrix filled with 0's and 1's,
+ * find the largest rectangle containing all ones and return its area.
+ */
+
+public class MaximalRectangle {
+    public int maximalRectangle(char[][] matrix) {
+        if (matrix == null || matrix.length == 0) {
+            return 0;
+        }
+
+        // The heights of each column from each row's point of view
+        int[][] heights = new int[matrix.length][matrix[0].length + 1];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == '1') {
+                    heights[i][j] = (i == 0) ? 1 : heights[i - 1][j] + 1;
+                }
+            }
+        }
+
+        // Get the maximal rectangle that can be obtained from each row's
+        // point of view, and then pick the maximal one
+        int max = 0;
+        for (int i = 0; i < heights.length; i++) {
+            max = Math.max(max, getRowMax(heights[i]));
+        }
+
+        return max;
+    }
+
+    /**
+     * Traditional maximal rectangle in histogram problem.
+     * 
+     * @param heights
+     * @return
+     */
+    private int getRowMax(int[] heights) {
+        int max = 0;
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < heights.length; i++) {
+            while (!stack.isEmpty() && heights[stack.peek()] > heights[i]) {
+                max = Math.max(max, heights[stack.pop()] * (i - (stack.isEmpty() ? 0 : stack.peek() + 1)));
+            }
+            stack.push(i);
+        }
+
+        return max;
+    }
+}
