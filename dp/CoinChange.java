d3182a2508049a5b75c1da84ef534352c2bddfa4
Xuli Liu <liuxuli@gmail.com>
2016-07-20 23:28:25 -0700

    Coin exchange

---
+++ dp/CoinChange.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+
+/**
+ * You are given coins of different denominations and a total amount of money amount.
+ * Write a function to compute the fewest number of coins that you need to make up that amount.
+ * If that amount of money cannot be made up by any combination of the coins, return -1.
+ *
+ * Example 1:
+ * coins = [1, 2, 5], amount = 11
+ * return 3 (11 = 5 + 5 + 1)
+ *
+ * Example 2:
+ * coins = [2], amount = 3
+ * return -1.
+ *
+ * Note:
+ * You may assume that you have an infinite number of each kind of coin.
+ */
+
+public class CoinChange {
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, 1, dp.length, Integer.MAX_VALUE);
+
+        // For each coin, check if the number can be reduced on top of the previous one.
+        for (int coin : coins) {
+            for (int i = coin; i <= amount; i++) {
+                if (dp[i - coin] != Integer.MAX_VALUE) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                }
+            }
+        }
+
+        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
+    }
+}
