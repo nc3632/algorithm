1c6e02b08df65ec8a1c0f94194fa130688d03276
Xuli Liu <liuxuli@gmail.com>
2016-07-21 21:38:56 -0700

    Distinct subsequence of t in s

---
+++ dp/DistinctSubsequence.java
@@ -0,0 +1,33 @@
+/**
+ * Given a string S and a string T, count the number of distinct subsequences of T in S.
+ *
+ * A subsequence of a string is a new string which is formed from the original string by deleting some (can be none)
+ * of the characters without disturbing the relative positions of the remaining characters.
+ * (ie, "ACE" is a subsequence of "ABCDE" while "AEC" is not).
+ *
+ * Here is an example:
+ * S = "rabbbit", T = "rabbit"
+ *
+ * Return 3.
+ */
+
+public class DistinctSubsequence {
+    public int numDistinct(String s, String t) {
+        int[][] dp = new int[s.length() + 1][t.length() + 1];
+        for (int i = 0; i < dp.length; i++) {
+            // Empty string counts as a substring of another
+            dp[i][0] = 1;
+        }
+
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[0].length; j++) {
+                dp[i][j] = dp[i - 1][j];
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    dp[i][j] += dp[i - 1][j - 1];
+                }
+            }
+        }
+
+        return dp[dp.length - 1][dp[0].length - 1];
+    }
+}
