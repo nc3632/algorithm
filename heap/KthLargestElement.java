e3e6cc77de01481d3e1b3b680dc22727e41fdc22
Xuli Liu <liuxuli@gmail.com>
2016-08-26 09:15:05 -0700

    Using heap to get the kth largest element in an array

---
+++ heap/KthLargestElement.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * Find the kth largest element in an unsorted array. Note that it is the kth largest element in the sorted order,
+ * not the kth distinct element.
+ *
+ * For example,
+ * Given [3,2,1,5,6,4] and k = 2, return 5.
+ *
+ * Note:
+ * You may assume k is always valid, 1 ≤ k ≤ array's length.
+ */
+
+public class KthLargestElement {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator() {
+            @Override
+            public int compare(Object o1, Object o2) {
+                Integer n1 = (Integer) o1;
+                Integer n2 = (Integer) o2;
+                return n2.compareTo(n1);
+            }
+        });
+
+        for(int n : nums) {
+            pq.offer(n);
+        }
+
+        int result = 0;
+        for (int i = 0; i < k; i++) {
+            result = pq.poll();
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        KthLargestElement sol = new KthLargestElement();
+        System.out.println(sol.findKthLargest(new int[]{3, 2, 1, 5, 6, 4}, 2));
+    }
+}
