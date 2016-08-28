c8463eb88ae580d79a88356798bd9a40df46a745
Xuli Liu <liuxuli@gmail.com>
2016-08-27 20:03:15 -0700

    Improvement

--- dp/LongestPalindromeSubstringDP.java
+++ dp/LongestPalindromeSubstringDP.java
@@ -10,48 +10,36 @@
 public class LongestPalindromeSubstringDP {
     public String longestPalindrome(String s) {
         boolean[][] dp = new boolean[s.length()][s.length()];
-        String result = null;
-        boolean found = false;
+        int start = 0;
+        int maxLen = 1;
 
         // Palindromic substring with length of 1
         for (int i = 0; i < dp.length; i++) {
             dp[i][i] = true;
-            if (!found) {
-                result = s.substring(i, i + 1);
-                found = true;
-            }
         }
 
         // Palindromic substring with length of 2
-        found = false;
         for (int i = 0; i < dp.length - 1; i++) {
             if (s.charAt(i) == s.charAt(i + 1)) {
                 dp[i][i + 1] = true;
-
-                if (!found) {
-                    result = s.substring(i, i + 2);
-                    found = true;
-                }
+                start = i;
+                maxLen = 2;
             }
         }
 
         // Palindromic substring with length of more than 2
         for (int k = 3; k <= dp.length; k++) {
-            found = false;
             for (int i = 0; i < dp.length - k + 1; i++) {
                 int j = i + k - 1;
                 if (dp[i + 1][j - 1] && s.charAt(i) == s.charAt(j)) {
                     dp[i][j] = true;
-
-                    if (!found) {
-                        result = s.substring(i, j + 1);
-                        found = true;
-                    }
+                    start = i;
+                    maxLen = k;
                 }
             }
         }
 
-        return result;
+        return s.substring(start, start + maxLen);
     }
 
     public static void main(String[] args) {
