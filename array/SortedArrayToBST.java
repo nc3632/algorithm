32a00a721ecb9fae99db3c5dbc48271fdf35e944
Xuli Liu <liuxuli@gmail.com>
2016-09-02 23:07:30 -0700

    Sorted array to BST

---
+++ array/SortedArrayToBST.java
@@ -0,0 +1,31 @@
+/**
+ * Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
+ */
+
+public class SortedArrayToBST {
+    private static class TreeNode {
+        public int val;
+        public TreeNode left, right;
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return sortedArrayToBST(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode sortedArrayToBST(int[] nums, int start, int end) {
+        if (nums == null || nums.length == 0 || start > end) {
+            return null;
+        }
+
+        int mid = start + (end - start) / 2;
+        TreeNode node = new TreeNode(nums[mid]);
+        node.left = sortedArrayToBST(nums, start, mid - 1);
+        node.right = sortedArrayToBST(nums, mid + 1, end);
+
+        return node;
+    }
+}
