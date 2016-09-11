b8ec4da0afae4424434f7818ff20b35120262fa8
Xuli Liu <liuxuli@gmail.com>
2016-09-11 12:42:37 -0700

    Check if two strings are anagram

---
+++ hash/IsAnagram.java
@@ -0,0 +1,48 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Given two strings s and t, write a function to determine if t is an anagram of s.
+ *
+ * For example,
+ * s = "anagram", t = "nagaram", return true.
+ * s = "rat", t = "car", return false.
+ *
+ * Note:
+ * You may assume the string contains only lowercase alphabets.
+ *
+ * Follow up:
+ * What if the inputs contain unicode characters? How would you adapt your solution to such case?
+ */
+
+public class IsAnagram {
+    public boolean isAnagram(String s, String t) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            char ch = t.charAt(i);
+            if (!map.containsKey(ch)) {
+                return false;
+            }
+            map.put(ch, map.get(ch) - 1);
+        }
+
+        for (int val : map.values()) {
+            if (val != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        IsAnagram sol = new IsAnagram();
+        System.out.println(sol.isAnagram("anagram", "nagaram"));
+        System.out.println(sol.isAnagram("rat", "car"));
+    }
+}
