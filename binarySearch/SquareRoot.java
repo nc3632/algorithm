fe05b27234f308e60696fb7944903d9c8a936b85
Xuli Liu <liuxuli@gmail.com>
2016-09-12 22:53:00 -0700

    Use Newton's algorithm to get the exact value

--- binarySearch/SquareRoot.java
+++ binarySearch/SquareRoot.java
@@ -3,19 +3,37 @@
  */
 
 public class SquareRoot {
-    public int mySqrt(int x) {
-        int start = 1, end = x;
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
+    private static int TIMES = 10;
+
+    public double mySqrt(int x) {
+        for (int i = 1, j = x; i <= j;) {
+            int mid = i + (j - i) / 2;
             if (x / mid >= mid && x / (mid + 1) < mid + 1) {
-                return mid;
+                return mySqrtNewton(x, mid);
             } else if (x / mid < mid) {
-                end = mid - 1;
+                j = mid - 1;
             } else {
-                start = mid + 1;
+                i = mid + 1;
             }
         }
 
         return 0;
+    }
+
+    private double mySqrtNewton(int n, int x0) {
+        // x^2 = n, f(x) = x^2 - n, f'(x) = 2x.
+        // f(x) = f(x0) + f'(x0)(x - x0) = x0^2 - n + 2x0(x - x0) = -x0^2 - n + 2x0x = 0
+        // x = (n + x0^2) / 2x0
+        double result = x0;
+        for (int i = 0; i < TIMES; i++) {
+            result = (n + result * result) / (2.0 * result);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        SquareRoot sol = new SquareRoot();
+        System.out.println(sol.mySqrt(10));
     }
 }
