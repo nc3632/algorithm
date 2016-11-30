920c5a99d18acdd4584e7eac75486a9508402588
Xuli Liu <liuxuli@gmail.com>
2016-11-29 22:44:12 -0800

    Inorder traversal of a binary tree

---
+++ stack/InorderTraversal.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Given a binary tree, return the inorder traversal of its nodes' values.
+ *
+ * For example:
+ * Given binary tree [1,null,2,3],
+ *   1
+ *    \
+ *     2
+ *    /
+ *   3
+ * return [1,3,2].
+ */
+
+public class InorderTraversal {
+    private static class TreeNode {
+        public int val;
+        public TreeNode left, right;
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public List<Integer> InorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        inorderTraversal(root, result);
+
+        return result;
+    }
+
+    public void inorderTraversal(TreeNode root, List<Integer> result) {
+        if (root == null) {
+            return;
+        }
+
+        inorderTraversal(root.left, result);
+        result.add(root.val);
+        inorderTraversal(root.right, result);
+    }
+
+    public List<Integer> inorderTraversal1(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        while (root != null || !stack.isEmpty()) {
+            if (root != null) {
+                stack.push(root);
+                root = root.left;
+            } else {
+                root = stack.pop();
+                result.add(root.val);
+                root = root.right;
+            }
+        }
+
+        return result;
+    }
+}
